c656f0ce57d066d5592c711512499fbd61ef0e42
Olly2t <gtagod359@gmail.com>
2025-02-01 14:10:36 +0000

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// JavaScript to handle button click event
+document.getElementById('clickButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'You clicked the button!';
+});
